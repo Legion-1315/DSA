a62ed1ebd455b9c558f3c893d4077f68fd1c1edc
Legion-1315 <rsdb1808@gmail.com>
2026-02-28 13:48:28 +0530

    Detect cycle using dfs using Topo sort

--- Graph/Detect cycle in a Directed Graph.cpp
+++ Graph/Detect cycle in a Directed Graph.cpp
@@ -38,3 +38,37 @@ class Solution {
     }
 };
 
+//Using Topo sort Kahn's Algorithm O(V+E) time O(V) space
+class Solution {
+  public:
+    bool isCyclic(int V, vector<vector<int>> &edges) {
+        // code here
+        int cnt=0;
+        vector<vector<int>>adj(V);
+        vector<int> inDegree(V,0);
+        for(auto i : edges) {
+            adj[i[0]].push_back(i[1]);
+            inDegree[i[1]]++;
+            cnt++;
+        }
+        
+        queue<int>q;
+        for(int i=0;i<V;i++) {
+            if(inDegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            for(auto i : adj[node]) {
+                inDegree[i]--;
+                cnt--;
+                if(inDegree[i] == 0) {
+                    q.push(i);
+                }
+            }
+        }
+        return cnt == 0 ? false : true;
+    }
+};
